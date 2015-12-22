17bf2d53ca0d5037a51c14e6904226dde5f4dd37
Ruoyan Qin <rq25@cornell.edu>
2015-12-21 23:41:41 -0500

    Before changing separate pages to content on one page.

--- lgc/settings.py
+++ lgc/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party
+    'crispy_forms',
      # my apps
     'newstudent',
     'upperclassman',
